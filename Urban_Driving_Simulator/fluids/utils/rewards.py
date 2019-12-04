d832114e49b5e0b7c15e6d345a8cfaaad6fed113
jamieposton <jposton@nevada.unr.edu>
2019-12-03 18:35:28 -0800

    Added reward metric weights

--- Urban_Driving_Simulator/fluids/utils/rewards.py
+++ Urban_Driving_Simulator/fluids/utils/rewards.py
@@ -1,3 +1,9 @@
+COLLISION_WEIGHT = 500.0
+LIVELINESS_WEIGHT = 1.0
+JERK_WEIGHT = 1.0
+TRAJ_WEIGHT = 1.0
+
+
 def path_reward(state):
     total_reward = 0
 
@@ -7,19 +13,20 @@ def path_reward(state):
     # Loop over every car
     for c in controlled_cars:
         # Calc collisions
-        collision = -500 if state.is_in_collision(c) else 0
+        collision = -1.0 if state.is_in_collision(c) else 0
 
         # get liveliness
-        liveliness = -1.0*c.total_time
+        liveliness = -1.0 * c.total_time
 
         # get jerk
-        jerk = -1.0*c.jerk
+        jerk = -1.0 * c.jerk
 
         # get traj_follow
         traj_follow = c.last_to_goal
 
         # calc reward
-        reward = collision + liveliness + jerk + traj_follow
+        reward = COLLISION_WEIGHT * collision + LIVELINESS_WEIGHT * liveliness + \
+                 JERK_WEIGHT * jerk + TRAJ_WEIGHT * traj_follow
 
         # update reward for car
         c.current_reward = reward
