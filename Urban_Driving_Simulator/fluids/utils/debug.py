42778991df4f4249f85f2ec866d1fb966e2c62f3
houstonlucas <houstonlucas@nevada.unr.edu>
2019-12-09 12:04:56 -0800

    Setup Neat running on a single scenario. Updated neat config file. Modified fluids_print function to never print.

--- Urban_Driving_Simulator/fluids/utils/debug.py
+++ Urban_Driving_Simulator/fluids/utils/debug.py
@@ -3,7 +3,9 @@ import sys
 
 
 def fluids_print(s, **kwargs):
-    print("[FLUIDS] " + str(s), **kwargs)
+    pass
+    # print("[FLUIDS] " + str(s), **kwargs)
+
 def fluids_assert(cond, em=None):
     if not cond:
         fluids_print("Error: " + em)
---
+++ fluids_neat.py
@@ -0,0 +1,82 @@
+
+import neat
+import numpy as np
+
+import visualize
+
+import os
+import gym
+import gym_fluids
+from matplotlib import pyplot as plt
+
+env_name = 'fluids-1-v2'
+
+
+def eval_genomes(genomes, config):
+    env = gym.make(env_name)
+    for genome_id, genome in genomes:
+        genome.fitness = 0.0
+        num_runs = 5
+        num_steps_per_run = 1000
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        for run_i in range(num_runs):
+            genome.fitness += single_run(net, env, num_steps_per_run)
+        genome.fitness /= num_runs
+
+
+def single_run(net, env, num_steps_per_run):
+    total_reward = 0.0
+    obs = env.reset()
+    for step_i in range(num_steps_per_run):
+        action = net.activate(obs)
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        if done:
+            break
+    return total_reward
+
+
+def run(config_file):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(20))
+
+    # Run for up to 300 generations.
+    winner = p.run(eval_genomes, 300)
+
+    # Display the winning genome.
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # node_names = {-1: 'A', -2: 'B', 0: 'A XOR B'}
+    # visualize.draw_net(config, winner, True, node_names=node_names)
+    visualize.plot_stats(stats, ylog=False, view=True)
+    visualize.plot_species(stats, view=True)
+    input("Press enter to view output")
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+
+    env = gym.make(env_name)
+    num_runs = 5
+    num_steps_per_run = 1000
+    total_reward = 0.0
+    for run_i in range(num_runs):
+        single_run(winner_net, env, num_steps_per_run)
+
+    print("Average reward per run: {}".format(total_reward/num_runs))
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward')
+    run(config_path)
