dcbc0bc81a2414bddfc82d57162f8aab13e356f2
houstonlucas <houstonlucas@nevada.unr.edu>
2019-12-16 08:55:10 -0800

    Added baselines to plots. Post fixed issue where fitness was scaled incorrectly.

--- generate_graphs.py
+++ generate_graphs.py
@@ -6,6 +6,26 @@ import os
 
 output_path = "graphs_output"
 data_path = "set_data"
+
+# Baseline with global knowledge
+baseline = {
+    "Fitness": 32.45,
+    "Infraction": 0.0,
+    "Collision": 0.0,
+    "Jerk": 34.13,
+    "Liveliness": 689.54,
+    "Trajectory_Following": 3230.32
+}
+
+# Baseline with local knowledge
+baseline2 = {
+    "Fitness": 26.87,
+    "Infraction": 0.0,
+    "Collision": 99.0,
+    "Jerk": 29.21,
+    "Liveliness": 690.66,
+    "Trajectory_Following": 3221.74
+}
 num_files = 2
 
 
@@ -29,14 +49,21 @@ def load_dataframe():
 
 
 def graph_metric(metric_name, df):
-    ax = sns.lineplot(x="generation", y="value", hue="type", data=df)
+    if metric_name == "Fitness":
+        df.value *= 6.0
+    df = df.rename(columns={"type": ""})
+    ax = sns.lineplot(x="generation", y="value", hue="", data=df)
     ax.set_title(metric_name)
+
+    ax.axhline(y=baseline[metric_name], ls=':', label="Baseline", color="Magenta")
+    ax.axhline(y=baseline2[metric_name], ls='--', label="Baseline2")
+    ax.legend(title="")
+
     ax.plot()
     file_name = metric_name + ".pdf"
     save_location = os.path.join(output_path, file_name)
     plt.savefig(save_location)
     plt.clf()
-
 
 
 if __name__ == '__main__':
